42c321786d98001d128efea4722fbbdc59ea71c6
woongsik <w.dev.bear@gmail.com>
2020-03-13 20:13:17 +0900

    firebase 셋팅

--- src/main.js
+++ src/main.js
@@ -3,7 +3,24 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
+import * as firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/database";
+
 Vue.config.productionTip = false;
+
+const firebaseConfig = {
+  apiKey: process.env.VUE_APP_APIKEY,
+  authDomain: process.env.VUE_APP_AUTHDOMAIN,
+  databaseURL: process.env.VUE_APP_DATABASEURL,
+  projectId: process.env.VUE_APP_PROJECTID,
+  storageBucket: process.env.VUE_APP_STORAGEBUCKET,
+  messagingSenderId: process.env.VUE_APP_MESSAGINGSENDERID,
+  appId: process.env.VUE_APP_APPID,
+  measurementId: process.env.VUE_APP_MEASUREMENTID
+};
+
+firebase.initializeApp(firebaseConfig);
 
 new Vue({
   router,
--- src/views/About.vue
+++ src/views/About.vue
@@ -3,3 +3,43 @@
     <h1>This is an about page</h1>
   </div>
 </template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "Home",
+  components: {},
+  created() {},
+  mounted() {
+    // console.log(">>>", this.$refs.getUrl);
+
+    // this.getTest().then(data => {
+    //   console.log("!!!!!!!!!!!!!", data);
+    // });
+
+    this.getAddrMaskInfo().then(data => {
+      console.log("????????????", data);
+    });
+    console.log("?");
+  },
+  methods: {
+    async getAddrMaskInfo() {
+      try {
+        /**
+          주소를 기준으로 해당 구 또는 동내에 존재하는 판매처 및 재고 상태 등의 판매 정보 제공.
+          예- '서울특별시 강남구' or '서울특별시 강남구 논현동'
+          ('서울특별시' 와 같이 '시'단위만 입력하는 것은 불가능합니다.)
+         */
+        return await axios.get("/storesByAddr/json", {
+          params: {
+            address: "서울특별시 강남구"
+          }
+        });
+      } catch (e) {
+        console.error(e);
+      }
+    }
+  }
+};
+</script>
